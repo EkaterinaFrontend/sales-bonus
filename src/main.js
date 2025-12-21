bbc75d92b86fdbd9c3bfdb8d13720cc5fa6d543f
Ekaterina Dubinina <yekaterina.dubinina.2001@inbox.ru>
2025-12-20 22:54:26 -0400

    данные №9

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@
  * @param _product 
  * @returns {number}
  */
-function calculateSimpleRevenue(purchase, _product) {
-   // @TODO: Расчёт прибыли от операции
-    const {discount, sale_price, quantity} = purchase;
-       return sale_price * quantity * (1 - discount / 100);
-}
+// function calculateSimpleRevenue(purchase, _product) {
+//    // @TODO: Расчёт прибыли от операции
+//     const {discount, sale_price, quantity} = purchase;
+//        return sale_price * quantity * (1 - discount / 100);
+// }
 
+function calculateSimpleRevenue(purchase, _product) {
+   const {sale_price, quantity} = purchase;
+   
+   return sale_price * quantity; 
+}
 /** 
  * @param index порядковый номер в отсортированном массиве
  * @param total общее число продавцов
@@ -87,23 +92,23 @@ function analyzeSalesData(data, options) {
      data.purchase_records.forEach(record => {
         const seller = sellerIndex[record.seller_id];
 
-        if (seller){
-            seller.sales_count += 1;
-            seller.revenue += record.total_amount;
-        }
+      
         record.items.forEach(item => {
             const product = productIndex[item.sku];// товар
 
+            if (seller && product) { 
             const cost = product.purchase_price * item.quantity;// Посчитать себестоимость
             const revenue = calculateRevenue(item, product);
             const profit = revenue - cost;
+            seller.revenue += revenue; 
             seller.profit += profit;
+           
 
-        if (!seller.products_sold[item.sku]) {/////АААААААААА
+        if (!seller.products_sold[item.sku]) {
             seller.products_sold[item.sku] = 0;
 }
             seller.products_sold[item.sku] += item.quantity;
-
+        }
      });
     });
 
